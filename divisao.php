b6c766b5efaf2d283c229651d23e786ed707e5ad
Ayrton Pereira <ayrton_pereira1996@hotmail.com>
2019-03-31 22:49:52 +0000

    Criacao do frame/content do jogo da tabuada

--- divisao.php
+++ divisao.php
@@ -1,17 +1,18 @@
 <!--This page will permit users play the game "Jogo da tabuada"-->
 <!DOCTYPE html>
 <html lang="pt-pt">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="_css/estilo.css"/>
-    <link rel="sortcut icon" type="image/x-icon" href="_images/icone.ico"/>  
+    <link rel="stylesheet" type="text/css" href="_css/estilo.css" />
+    <link rel="sortcut icon" type="image/x-icon" href="_images/icone.ico" />
     <title>Jogo da Divisão!</title>
 
     <style>
-    body{
-        background: url("_images/wood-411643.png");
+    body {
+        background: url("_images/painter-3177366.png");
         background-repeat: no-repeat;
         background-size: 1400px 800px;
     }
@@ -20,12 +21,18 @@
 
 
 </head>
+
 <body>
     <div id="interface">
+        <h1>Jogo da Divisão!</h1>
         <div id="content-divisao">
-            <h1>Jogo da Divisão!</h1>
+            <p><span class="special-word4">Bem vindo</span> ao <span class="special-word2">Jogo da Divisão</span><span class="special-word">!</span><br/>
+            <span class="special-word3">Descobre</span> como <span>funciona</span> uma <span class="special-word">operação de divisão</span><span class="special-word4">.</span>
+        
+        </p>
         </div>
     </div>
-    
+
 </body>
+
 </html>
