9ad3687aa1834e8422e15c2b59c503cd66136712
Ayrton Pereira <ayrton_pereira1996@hotmail.com>
2019-05-22 14:34:56 +0000

    alteracao do footer

--- index.php
+++ index.php
@@ -8,19 +8,21 @@
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="sortcut icon" href="_images/icone.ico" type="image/x-icon"/>
             <link rel="stylesheet" type="text/css" href="_css/estilo.css"/>
+         
 
         </head>
             <body id="index">
                 <div id="interface">
+                
                   
                 <p style="margin-left:4%;margin-top:2%;color:rgb(77, 76, 76); font-weight:normal; text-shadow: -0.5px -0.5px 2.5px white; font-size: 5em; "><span class="special-word">J</span>ogo<span class="special-word4">s</span> <span class="special-word2">D</span>id<span class="special-word3">á</span>tico<span class="special-word4">s</span></p>
                    <div id="content-index">
                         <a href="main_page.php">&rarr; Entra para Jogar &larr;</a>
                    </div> 
                     
-                    <footer id="footer">
-                      <p>Developed at 2019</p>
-                    </footer>
+                  <div style="background-color:rgba(0, 0, 0, 0.527); margin-top: 255px; padding:5px;">
+                      <p style="font-size: 1em;color:whitesmoke;margin-left: 5%;text-align: center; ">Developed at 2019</p>
+                  </div>
                   
                 </div>
             </body>
