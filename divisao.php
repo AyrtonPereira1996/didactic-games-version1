9d66c0dc2cceb31d16e4cb39bb9c59e42ec9255f
Ayrton Pereira <ayrton_pereira1996@hotmail.com>
2019-05-27 09:45:21 +0000

    criacao dos campos de submissão

--- divisao.php
+++ divisao.php
@@ -11,11 +11,11 @@
     <title>Jogo da Divisão!</title>
 
     <style>
-    body {
-        background: url("_images/painter-3177366.png");
-        background-repeat: no-repeat;
-        background-size: 1400px 800px;
-    }
+        body {
+            background: url("_images/painter-3177366.png");
+            background-repeat: no-repeat;
+            background-size: 1400px 800px;
+        }
     </style>
 
 
@@ -24,13 +24,30 @@
 
 <body>
     <div id="interface">
-        <h1>Jogo da Divisão!</h1>
-        <div id="content-divisao">
-            <p><span class="special-word4">Bem vindo</span> ao <span class="special-word2">Jogo da Divisão</span><span class="special-word">!</span><br/>
-            <span class="special-word3">Descobre</span> como <span>funciona</span> uma <span class="special-word">operação de divisão</span><span class="special-word4">.</span>
-        
-        </p>
-        </div>
+        <center>
+            <h1>Jogo da Divisão!</h1>
+            <div id="content-divisao">
+                <p><span class="special-word4">Bem vindo</span> ao <span class="special-word2">Jogo da Divisão</span><span class="special-word">!</span><br />
+                    <span class="special-word3">Descobre</span> como <span>funciona</span> uma <span class="special-word">operação de divisão</span><span class="special-word4">.</span>
+                </p>
+
+                <form method="get" action="">
+                    <table>
+                        <tr>
+                            <th>Dividendo<br />&darr;</th>
+                            <th>Divisor<br />&darr;</th>
+                            <th>Quociente<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&darr;</th>
+                        </tr>
+                        <tr>
+                            <td><input type="number" name="dividendo" /> : </td>
+                            <td><input type="number" name="divisor" /></td>
+                            <td>= &nbsp;&nbsp;?</td>
+                        <tr>
+                    </table>
+                    <input class="button-play" type="submit" value="JOGA!" />
+                </form>
+            </div>
+        </center>
     </div>
 
 </body>
